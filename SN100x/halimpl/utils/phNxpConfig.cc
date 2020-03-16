853e3dc49711035f21a0dd2b381d6253a3ac6617
Tapas Dey <tdey@codeaurora.org>
2020-03-16 19:29:14 +0530

    NFC: Add dynamic config support for lagoon variant
    Add dynamic configuration support in HAL layer
    for lagoon variant platform to pick Nfc
    configuration file at runtime based
    on hw platform.
    
    Change-Id: I852fd105c98219fb3a6de51e1d0c3357c9fbff06

--- SN100x/halimpl/utils/phNxpConfig.cc
+++ SN100x/halimpl/utils/phNxpConfig.cc
@@ -151,6 +151,7 @@ typedef enum
   TARGET_SM8250                        = 356, /**< SM8250 target */
   TARGET_SM7250                        = 400, /**< SM7250 target */
   TARGET_SM6125                        = 394, /**< SM6125 target */
+  TARGET_SM_LAGOON_H                   = 459, /**< SM_LAGOON_H target */
   TARGET_LAGOON                        = 434, /**< LAGOON target */
   TARGET_BENGAL                        = 417, /**< BENGAL target */
   TARGET_SM_BENGAL_H                   = 444, /**< SM_BENGAL_H target */
@@ -432,6 +433,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM8150_SDx55:
         case TARGET_SM6125:
         case TARGET_LAGOON:
+        case TARGET_SM_LAGOON_H:
             config_id = QRD_TYPE_SN100;
             strlcpy(config_file, config_name_qrd_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
@@ -500,6 +502,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM8150_SDx55:
         case TARGET_SM6125:
         case TARGET_LAGOON:
+        case TARGET_SM_LAGOON_H:
             config_id = MTP_TYPE_SN100;
             strlcpy(config_file, config_name_mtp_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
